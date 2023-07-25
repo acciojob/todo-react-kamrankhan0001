0ddc909d924f7a06d115bf5f472a294cfc4a2da5
kamrankhan0001 <kamranbgp15@gmail.com>
2023-07-25 22:40:22 +0530

    submitting code-Tue Jul 25 2023 22:35:55 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,50 @@
 
-import React from "react";
+import React, { useState } from 'react';
 import './../styles/App.css';
 
 const App = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputValue.trim() !== '') {
+      setTodos([...todos, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  const handleRemoveTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h2>To-Do List</h2>
+      <div>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Enter your task..."
+        />
+        <button onClick={handleAddTodo}>Add</button>
+      </div>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleRemoveTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
+
 
 export default App
